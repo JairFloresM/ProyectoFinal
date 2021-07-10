2cc9b23d70e07668b851b4d6f40f73a3c671ec34
Draconis09 <e.leah2110@gmail.com>
2021-07-10 17:35:29 -0500

    Crear interfaz de Registrar Admin

--- Proyecto/src/Principal/Administrador.java
+++ Proyecto/src/Principal/Administrador.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal;
 
-/**
- *
- * @author Usuario
- */
 public class Administrador {
+    
+    private String nombre, email, contraseña;
+
+    public Administrador(String nombre, String email, String contraseña) {
+        this.nombre = nombre;
+        this.email = email;
+        this.contraseña = contraseña;
+    }
+    
+    
     
 }
